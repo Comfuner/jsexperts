758f2ba61e9f781c28c2c664ca4277d31897a6fd
Comfuner <elijahu.malka@gmail.com>
2018-01-02 00:01:42 +0200

    first lesson

--- local_storage-exe1/js/app.js
+++ local_storage-exe1/js/app.js
@@ -1,44 +1,43 @@
 (function(){
         // updating the range input value visually
-        var ageRange = document.getElementById('age')
+        let ageRange = document.getElementById('age');
         ageRange.setAttribute('data-value', ageRange.value);
         ageRange.addEventListener('input',function() {
             this.setAttribute('data-value', this.value);
         });
-
         // declaring storage name and content
-        var storageName = 'tempDB';
-        var localStorageContent = JSON.parse(localStorage.getItem(storageName)) || [];
+        let storageName = 'tempDB';
+        let localStorageContent = JSON.parse(localStorage.getItem(storageName)) || [];
         // print the list on load
         printLi();
 
         function UpdateDb() {
-            var tempObj = {
+            let tempObj = {
                 name : document.getElementById('name').value,
                 color : document.getElementById('color').value,
                 date :document.getElementById('date').value,
                 age: document.getElementById('age').value
-            }
+            };
 
             localStorageContent.push(tempObj);
-            var localStorageContentStr = JSON.stringify(localStorageContent)
+            let localStorageContentStr = JSON.stringify(localStorageContent);
             localStorage.setItem(storageName,localStorageContentStr);
-            console.log('Local storage has been updated!')
+            console.log('Local storage has been updated!');
             // print the list on update
             printLi();
         }
 
         function printLi() {
-            var theList = document.getElementById('thelist');
+            let theList = document.getElementById('thelist');
             theList.innerHTML="";
             localStorageContent.map(function (item) {
-                var tempLi = document.createElement('li');
+                let tempLi = document.createElement('li');
                 tempLi.innerHTML = item.name + ", " + item.age + ", " + item.date;
                 tempLi.style.color = item.color;
                 theList.appendChild(tempLi)
             })
         }
 
-    var sendButton = document.getElementById("sendTo");
+    let sendButton = document.getElementById("sendTo");
     sendButton.addEventListener('click', UpdateDb)
 })();
